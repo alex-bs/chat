eed274685450f5e7b5b61c6276fff1c351302f59
Alex Ardevol <aardevol@omnios.ai>
2020-09-11 18:24:42 +0200

    cors twitch

--- public/js/main.js
+++ public/js/main.js
@@ -7,5 +7,5 @@ socket.on('enter room', function(roomId){
 
 socket.on('message', function(name, message){
 	var iframe = document.getElementById('streamIframe');
-	iframe.src = 'https://player.twitch.tv/?'+message+'&parent=streamernews.example.com';
+	iframe.src = 'https://player.twitch.tv/?'+message+'&parent=twitchchat3-env.eba-dtg8awxh.us-west-2.elasticbeanstalk.com';
 })
